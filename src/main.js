44fefa71cc5d7712822f4f5554ae866315c5998f
joseph1997520 <joseph1997520@gmail.com>
2022-08-09 15:58:35 +0800

    點選list更換中心座標

--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 
 const app = createApp(App);
+library.add(faUserSecret , faLocationDot )
 
+app.component('fa' , FontAwesomeIcon)
 app.use(router),
 app.use(VueAxios, axios),
 app.mount('#app')
